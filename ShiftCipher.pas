b01e497ab279ab03ee75cba82719b2264fd9d465
ohaikenton <kentonli622@gmail.com>
2019-11-23 22:53:51 +0800

    Markup

--- ShiftCipher.pas
+++ ShiftCipher.pas
@@ -1,4 +1,4 @@
-(* Created by Li Chun Kit. © 2019 Li Chun Kit . All rights reserved.
+(* Created by Li Chun Kit. © 2019 Li Chun Kit. 
   Compiler: FPC v3.0.4
   IDE: Lazarus, Visual Studio Code *)
 program ShiftCipher;
